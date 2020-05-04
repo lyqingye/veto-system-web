bb7087dbf9216efa7dbc7b2fb1ad8b36ba90cb1b
lyqingye <1316515719@qq.com>
2020-05-04 23:40:12 +0800

    save

--- src/views/login/register.vue
+++ src/views/login/register.vue
@@ -3,7 +3,7 @@
     <el-form ref="registerForm" :model="registerForm" :rules="loginRules" class="login-form" autocomplete="on" label-position="left">
 
       <div class="title-container">
-        <h3 class="title">Login Form</h3>
+        <h3 class="title">用户注册</h3>
       </div>
 
       <el-form-item prop="mobile">
